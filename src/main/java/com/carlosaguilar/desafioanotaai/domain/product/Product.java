03e46d08f7a6e57d2f1d86c1c25255bc2386e8a7
Carlos Eduardo Teixeira Aguilar <taguilar2504@gmail.com>
2024-08-07 16:50:55 -0300

    Product Model

---
+++ src/main/java/com/carlosaguilar/desafioanotaai/domain/product/Product.java
@@ -0,0 +1,26 @@
+package com.carlosaguilar.desafioanotaai.domain.product;
+
+import com.carlosaguilar.desafioanotaai.domain.category.Category;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    private String id;
+
+    private String title;
+    private String description;
+    private String ownerId;
+
+    private Integer price;
+    private Category category;
+
+}
