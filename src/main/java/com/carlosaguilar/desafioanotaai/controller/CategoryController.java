9531ae5490b5f95de1fc4c5ca97aaf31308ee584
Carlos Eduardo Teixeira Aguilar <taguilar2504@gmail.com>
2024-08-07 23:00:02 -0300

    feat: add endpoint to get all categories

--- src/main/java/com/carlosaguilar/desafioanotaai/controller/CategoryController.java
+++ src/main/java/com/carlosaguilar/desafioanotaai/controller/CategoryController.java
@@ -5,10 +5,9 @@ import com.carlosaguilar.desafioanotaai.domain.category.dto.CategoryDTO;
 import com.carlosaguilar.desafioanotaai.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -17,11 +16,16 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-
     @PostMapping
     public ResponseEntity<Category> insert(@RequestBody CategoryDTO categoryDTO) {
         Category category = this.categoryService.insert(categoryDTO);
         return ResponseEntity.ok().body(category);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAll() {
+        List<Category> categories = this.categoryService.getAll();
+        return ResponseEntity.ok().body(categories);
     }
 
 }
--- src/main/java/com/carlosaguilar/desafioanotaai/service/CategoryService.java
+++ src/main/java/com/carlosaguilar/desafioanotaai/service/CategoryService.java
@@ -3,9 +3,10 @@ package com.carlosaguilar.desafioanotaai.service;
 import com.carlosaguilar.desafioanotaai.domain.category.Category;
 import com.carlosaguilar.desafioanotaai.domain.category.dto.CategoryDTO;
 import com.carlosaguilar.desafioanotaai.repositories.CategoryRepository;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -13,11 +14,14 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
-
     public Category insert(CategoryDTO categoryDTO) {
         Category category = new Category(categoryDTO);
         this.categoryRepository.save(category);
         return category;
+    }
+
+    public List<Category> getAll() {
+        return this.categoryRepository.findAll();
     }
 
 }
