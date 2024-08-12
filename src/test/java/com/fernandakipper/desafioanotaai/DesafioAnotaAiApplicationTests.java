6625a51ab99e42fa31e8afb0f4b63df585875239
Carlos Eduardo Teixeira Aguilar <taguilar2504@gmail.com>
2024-08-11 21:51:23 -0300

    Implementing JUnit Tests

--- src/test/java/com/fernandakipper/desafioanotaai/DesafioAnotaAiApplicationTests.java
+++ src/test/java/com/fernandakipper/desafioanotaai/DesafioAnotaAiApplicationTests.java
@@ -1,9 +1,11 @@
-package com.carlosaguilar.desafioanotaai;
+package com.fernandakipper.desafioanotaai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DesafioAnotaAiApplicationTests {
 
 	@Test
